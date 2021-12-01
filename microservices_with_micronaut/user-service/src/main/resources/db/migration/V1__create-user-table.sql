a1c67c89c70aaaaf57f5de248cbfdb429593dc48
Amit Bhave <amitbhave10@gmail.com>
2021-12-01 23:11:14 +0530

    Database migrations with Flyway

---
+++ microservices_with_micronaut/user-service/src/main/resources/db/migration/V1__create-user-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) DEFAULT NULL,
+    `email` varchar(255) DEFAULT NULL,
+    `mobile_number` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)
